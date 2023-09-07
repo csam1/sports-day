bfb61fb40cc825d7f268b36afea3604ec968243d
Sam <cpsamyamasam@gmail.com>
2023-09-07 10:53:21 +0530

    added localstorage support

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,3 @@
 export const SELECTED_EVENTS_LIMIT = 3;
+
+export const USER_NAME = "user";
--- src/reducer/eventsReducer.ts
+++ src/reducer/eventsReducer.ts
@@ -2,7 +2,7 @@ import { createContext } from 'react';
 
 import { EventActionTypes } from '../types/Action';
 import { EventProperties } from '../types/event';
-import { SELECTED_EVENTS_LIMIT } from '../constants';
+import { SELECTED_EVENTS_LIMIT, USER_NAME } from '../constants';
 
 type StateProps = {
   eventList: EventProperties[];
@@ -31,21 +31,35 @@ export const GlobalContext = createContext<GlobalContent>({
 export const eventReducer = (state: StateProps, action: ActionTypes): StateProps => {
   switch (action.type) {
     case EventActionTypes.UPDATE_EVENT_LIST: {
+      const storage = localStorage.getItem("selectedEvents");
+      let eventList=action.eventList;
+      let selectedEvents=[] ;
+      if(storage){
+        selectedEvents = JSON.parse(storage);
+        let ids = selectedEvents[USER_NAME].events.map((event:EventProperties)=>event.id);
+        eventList = action.eventList?.filter((event)=>!ids.includes(event.id))
+      }
       return {
-        eventList: action?.eventList || [],
-        selectedEventList: [],
+        eventList: eventList || [],
+        selectedEventList: selectedEvents[USER_NAME].events,
       };
     }
     case EventActionTypes.DESELECT_EVENT: {
+      const selectedEventList = state.selectedEventList.filter((event)=>event.id !== action?.event?.id);
+      const setEvents = {[USER_NAME]: {events: selectedEventList}}
+      localStorage.setItem('selectedEvents',JSON.stringify(setEvents));
       return {
-        selectedEventList: state.selectedEventList.filter((event)=>event.id !== action?.event?.id),
+        selectedEventList: selectedEventList,
         eventList: state.eventList.concat(action?.event||[])
       };
     }
     case EventActionTypes.SELECT_EVENT: {
       if (state.selectedEventList.length < SELECTED_EVENTS_LIMIT) {
+        const selectedEventList = state.selectedEventList.concat(action?.event || []);
+        let setEvents = {[USER_NAME]: {events: selectedEventList}};
+        localStorage.setItem('selectedEvents',JSON.stringify(setEvents));
         return {
-          selectedEventList: state.selectedEventList.concat(action?.event || []),
+          selectedEventList,
           eventList: state.eventList.filter((event)=>event.id !== action?.event?.id)
         };
       }
